812f413b251af16e9a44ee33ccbaab73092e5970
Diego <101619179+Dieg0MV@users.noreply.github.com>
2023-06-07 05:28:37 +0000

    Update whats_aut.py

--- whats_aut.py
+++ whats_aut.py
@@ -2,34 +2,37 @@ import xlrd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 
 
 class Logica:
     def __init__(self):
-        self.driver = webdriver.Edge()
+
+        self.servic = Service('/home/mrhead/Desktop/whats_app/chromedriver')
+        self.driver = webdriver.Chrome(service=self.servic)
         self.start_url = 'https://web.whatsapp.com'
 
         self.driver.get(self.start_url)
 
-        time.sleep(20)
+        time.sleep(50)
 
         # De aquí sacamos los números
-        self.excelFile = xlrd.open_workbook("num.xls")
+        self.excelFile = xlrd.open_workbook("name.xls")
         self.sheet1 = self.excelFile.sheet_by_index(0)
 
         for i in range(1, self.sheet1.nrows):
 
             # Elementos del campo de búsqueda
-            numeros = int(self.sheet1.cell_value(rowx=i, colx=0))
-            file_s = self.driver.find_element(By.CLASS_NAME, 'selectable-text.copyable-text')
+                numeros = self.sheet1.cell_value(rowx=i, colx=0)
+                file_s = self.driver.find_element(By.CLASS_NAME, 'selectable-text.copyable-text')
 
-            try:
+
                 file_s.send_keys(numeros)
                 time.sleep(1)
                 file_s.send_keys(Keys.ENTER)
 
-                # Esperar unos segundos para asegurarse de que se haya abierto la conversación
+            # Esperar unos segundos para asegurarse de que se haya abierto la conversación
                 time.sleep(2)
 
                 # Enviar el mensaje utilizando pywhatkit
@@ -39,14 +42,9 @@ class Logica:
                 time.sleep(1)
                 chat_box.send_keys("Mensaje")
                 chat_box.send_keys(Keys.ENTER)
+                time.sleep(5)
 
-            except Exception as e:
-                print('No se envió')
-                de = self.driver.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/div/span/button/span')
-                de.click()
-                continue
-
-        # Cerrar el navegador
+            # Cerrar el navegador
         self.driver.quit()
 
 
